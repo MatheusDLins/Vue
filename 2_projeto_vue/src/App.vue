63612ecc15ed8baf8857b3399cc1999757ad0a2f
MatheusDLins <matheus934qwer@gmail.com>
2024-08-25 12:05:34 -0300

    reutilizando componentes

--- 2_projeto_vue/src/App.vue
+++ 2_projeto_vue/src/App.vue
@@ -1,14 +1,29 @@
 <template>
-  <PrimeiroComponente/>
+<div>
+  <PrimeiroComponente />
+  <P>O dado de app é: {{ testando }}</P>
+  <LifeCycle />
+  <Pessoa />
+</div>
 </template>
 
 <script>
 import PrimeiroComponente from './components/PrimeiroComponente.vue'
+import LifeCycle from './components/LifeCycle.vue'
+import Pessoa from './components/Pessoa.vue'
+
 
 export default {
   name: 'App',
   components:{
-    PrimeiroComponente
-  }
+    PrimeiroComponente,
+    LifeCycle,
+    Pessoa
+  },
+        data() {
+            return{
+                testando: "teste"
+            }
+        }
 }
 </script>
